2eba2412172a1cb0e37ebfeff369aa34b4cf0112
Abigail Buccaneer <abigail.buccaneer@gmail.com>
2014-07-20 09:06:17 +0100

    --- yaml --- svn_rev: 116 current_ref: refs/heads/master current_commit: 28d4bf73a8368e475f81f6bf8f9633fc6ff00e90 head_branch: refs/heads/master migrated_from: v3

--- trunk/lua/weapons/gmod_tool/stools/advdupe2.lua
+++ trunk/lua/weapons/gmod_tool/stools/advdupe2.lua
@@ -40,12 +40,10 @@ if(SERVER)then
 	//Find all the entities in a box, given the adjacent corners and the player
 	local function FindInBox(min, max, ply)
 
-		local Entities = ents.GetAll()
+		local Entities = ents.FindInBox(min, max)
 		local EntTable = {}
-		local pos
 		for _,ent in pairs(Entities) do
-			pos = ent:GetPos()
-			if (pos.X>=min.X) and (pos.X<=max.X) and (pos.Y>=min.Y) and (pos.Y<=max.Y) and (pos.Z>=min.Z) and (pos.Z<=max.Z) and duplicator.IsAllowed(ent:GetClass()) then
+			if duplicator.IsAllowed(ent:GetClass()) then
 				local trace = WireLib and WireLib.DummyTrace(ent) or { Entity = ent }
 				if hook.Run( "CanTool", ply,  trace, "advdupe2" ) then
 					EntTable[ent:EntIndex()] = ent
