48a34aa37c516a21e0572b96b6098b642c630723
Mista-Tea <thatguythomas@hotmail.com>
2016-11-11 16:16:47 -0600

    Make paste angles default to 0,0,0 (#104)

--- lua/weapons/gmod_tool/stools/advdupe2.lua
+++ lua/weapons/gmod_tool/stools/advdupe2.lua
@@ -84,7 +84,7 @@ if(SERVER)then
 		
 		local z = math.Clamp((tonumber(ply:GetInfo("advdupe2_offset_z")) + ply.AdvDupe2.HeadEnt.Z), -16000, 16000)
 		ply.AdvDupe2.Position = trace.HitPos + Vector(0, 0, z)
-		ply.AdvDupe2.Angle = Angle(tonumber(ply:GetInfo("advdupe2_offset_pitch")), tonumber(ply:GetInfo("advdupe2_offset_yaw")), tonumber(ply:GetInfo("advdupe2_offset_roll")))
+		ply.AdvDupe2.Angle = Angle(ply:GetInfoNum("advdupe2_offset_pitch", 0), ply:GetInfoNum("advdupe2_offset_yaw", 0), ply:GetInfoNum("advdupe2_offset_roll", 0))
 		if(tobool(ply:GetInfo("advdupe2_offset_world")))then ply.AdvDupe2.Angle = ply.AdvDupe2.Angle - ply.AdvDupe2.Entities[ply.AdvDupe2.HeadEnt.Index].PhysicsObjects[0].Angle end
 		
 		ply.AdvDupe2.Pasting = true
