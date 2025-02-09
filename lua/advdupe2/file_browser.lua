44914cc6dadd95880461b68ff4be226e7c8a1fc5
Rubik <sbsrubik@outlook.com>
2025-02-09 12:10:24 -0500

    Update file_browser.lua
    This commit addresses a Lua error introduced with https://github.com/wiremod/advdupe2/pull/495
    
    Sorry!

--- lua/advdupe2/file_browser.lua
+++ lua/advdupe2/file_browser.lua
@@ -172,6 +172,12 @@ function BROWSER:DoNodeLeftClick(node)
 			if (node.Expander) then
 				node:SetExpanded() -- It's a folder, expand/collapse it
 			end
+		elseif (node.Derma.ClassName == "advdupe2_browser_file") then
+			if (node.Control.Search) then
+				AdvDupe2.UploadFile(GetNodePath(node.Ref))
+			else
+				AdvDupe2.UploadFile(GetNodePath(node))
+			end
 		else
 			AdvDupe2.UploadFile(GetNodePath(node))
 		end
