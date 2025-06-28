7ab20de52be60e57a063321c8cfc040f5e390b2c
march <106459595+marchc1@users.noreply.github.com>
2025-06-28 13:25:43 -0700

    Move GetFilename to its own file
    Not sure if this is even used serverside and needs to be shared...

--- lua/advdupe2/file_browser.lua
+++ lua/advdupe2/file_browser.lua
@@ -774,17 +774,6 @@ local function CollapseParentsComplete(node)
 	CollapseParentsComplete(node.ParentNode)
 end
 
-function AdvDupe2.GetFilename(path, overwrite)
-	if not overwrite and file.Exists(path .. ".txt", "DATA") then
-		for i = 1, AdvDupe2.FileRenameTryLimit do
-			local p = string.format("%s_%03d.txt", path, i)
-			if not file.Exists(p, "DATA") then
-				return p
-			end
-		end
-		return false
-	end
-	return path .. ".txt"
 end
 
 local function RenameFileCl(node, name)
