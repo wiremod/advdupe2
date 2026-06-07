a5d82db1492505651a039fdb01421ec2c833ede1
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2026-06-07 16:45:56 +0300

    Flags support for AdvDupe2 rendering (#543)
    Supplement to https://github.com/wiremod/wire/pull/3624

--- lua/entities/gmod_contr_spawner/cl_init.lua
+++ lua/entities/gmod_contr_spawner/cl_init.lua
@@ -1,6 +1,6 @@
 include( "shared.lua" )
 
-function ENT:Draw()
-	self.BaseClass.Draw(self)
-	self.Entity:DrawModel()
+function ENT:Draw(flags)
+	self.BaseClass.Draw(self, flags)
+	self.Entity:DrawModel(flags)
 end
